ffeebfb0dd512d2a0585b113986ac95a8072eb39
andrewchester1 <49591047+andrewchester1@users.noreply.github.com>
2021-10-01 22:28:19 -0500

    finished tab bar, setting up ui

--- AppStack.tsx
+++ AppStack.tsx
@@ -6,8 +6,40 @@ import LoadingScreen from "./screens/LoadingScreen";
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import FirebaseApp from "./FirebaseApp";
+import AboutScreen from "./screens/AboutScreen";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+const Tab = createBottomTabNavigator()
+
+function MainStackNavigator() {
+    return (
+        <Tab.Navigator screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+  
+              if (route.name === 'Home') {
+                iconName = focused
+                  ? 'ios-information-circle'
+                  : 'ios-information-circle-outline';
+              } else if (route.name === 'About') {
+                iconName = focused ? 'ios-list-box' : 'ios-list';
+              }
+  
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+            <Tab.Screen name='Home' component={HomeScreen} />
+            <Tab.Screen name='About' component={AboutScreen} />
+        </Tab.Navigator>
+    )
+}
 const Stack = createNativeStackNavigator ();
+
 export default function AppStack() {
     const { user, isLoading } = useContext(LoginContext);
     return (
@@ -16,7 +48,7 @@ export default function AppStack() {
                 {isLoading ? ( 
                     <Stack.Screen name="loading" options={{ headerShown: false}} component={LoadingScreen}/> 
                 ): user ? (
-                <Stack.Screen name="Home" component={HomeScreen}/> 
+                <Stack.Screen name="Home" options={{ headerShown: false}} component={MainStackNavigator} /> 
                 ) : (
                 <Stack.Screen name="signin" component={LoginScreen}/>
                 )}
--- screens/AboutScreen.tsx
+++ screens/AboutScreen.tsx
@@ -6,8 +6,6 @@ import FirestoreUtil from '../utils/FirestoreUtil';
 import { LoginContext } from '../utils/LoginProvider';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import LoginScreen from './LoginScreen';
-import AboutScreen from './AboutScreen';
 
 const HomeScreen = () => {
     const { user } = useContext(LoginContext);
@@ -20,16 +18,7 @@ const HomeScreen = () => {
 
     return(
         <View style={styles.container}>
-            <View style={styles.box}>
-                <FirebaseApp />
-            </View>
-            <View style={styles.box}>
-                <Text>Item 2</Text>
-            </View>
-            <View style={styles.box}>
-                <Text>Item 3</Text>
-                <Button onPress={() => signOut()} title='Logout' />
-            </View>
+            <Button onPress={()=> signOut()} title='Logout'/>
         </View>
     )
 }
@@ -37,16 +26,11 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-    //   backgroundColor: '#fff',
+      backgroundColor: '#fff',
       justifyContent: 'center',
       alignContent: 'center',
-      padding: 20,
-    },
-    box: {
-        flex: 1,
-        backgroundColor: "grey",
-        padding: 20,
-      }
+      padding: 20
+    }
   });
 
 export default HomeScreen
